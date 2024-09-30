47ba89b790784c04158edf5c17d6dcb004c1dc94
Satyam Jaiswal <satyamjaiswal9752@gmail.com>
2024-10-01 01:17:09 +0530

    llm

---
+++ server/utils/pdfread.py
@@ -0,0 +1,58 @@
+from PyPDF2 import PdfReader
+
+def pdf_read():
+    # text = ""
+    # pdf_reader = PdfReader('satyam_resume.pdf')
+    # for page in pdf_reader.pages:
+    #     text += page.extract_text()
+    return """Vikas Rajpurohit
+/gtbVikas-Rajpurohit | code-byte | vikasrazz158@gmail.com | /ne+91-9167543560
+Work Experience
+Google Summer Of Code’24 May 2024 - Present
+The University Of Alaska - Open Source Contributor Remote
+•Developed a real-time forest fire management system that provides critical insights to forest fire rangers.
+•Integrated various YOLO, D2Go OBB models for early detection and advanced processing algorithms to
+calculate the area covered by fire, direction of wind and fire spread using optical-flow andimage processing .
+Smowcode Nov 2023 - Feb 2024
+ML Engineer, Internship Mumbai, India
+•Directed the Voice Command Automation project leveraging IoT technologies .
+•Researched over 10+ language models having 3B and 7B parameters forcode generation andQ&A tasks .
+•Finetuned 7B Mistral Language Model , cutting development time from300 secs to 10 secs :Results
+Technical Skills
+Languages: Python, C++, Javascript, C, SQL
+ML/AI:Tensorflow, HuggingFace, Langchain, MLFlow, OpenCV,Numpy, Pandas, Matplotlib, ScikitLearn
+Frontend: HTML, CSS, React, TailwindCSS
+Backend: Flask, Node.js, Express.js, Socket.IO, MySQL, MongoDB
+Version Control: Git, Github
+Education
+Sardar Patel Institute of Technology Dec 2021 - May 2025
+Bachelor of Technology in Computer Science and Engineering CGPA: 8.1
+Relevant Projects
+Ettara Cafe (Streamlining Feedback, Analysis, and Inventory Processes) [Role: AI ML & Backend] Feb 2024
+HackNiche’24 repo: Ettara Cafe
+•Future sales projections using the ARIMA model and developed a data analytics dashboard for orders analysis.
+•Personalized coffee recommendations and aninteractive AR view for coffee takeaway orders.
+•Integrateda GooglePaLMandLangchainSQL-Chain forqueryingdatabaseinnaturallanguageand WhatsApp
+botfor seamless order placements and customer feedback.
+•Performed competitor analysis ,rating trend analysis , and developed an automated improvements checklist
+that dynamically updates based on customer feedback integration.
+Medi-Care (Summarize and Query your Medical reports) [Role: AI ML & Backend] Jan 2024
+TSEC Hacks’24 repo: Medi-Care
+•Replaced token chunking with metadata-rich medical test-specific chunks for efficient data retrieval.
+•Enhanced report summarization with individual test summaries for a precise final report .
+•Integrated a fine-tuned Microsoft/Phi-2 3B LLM for accurate responses to complex medical report queries.
+•Employed LangChain for specialized LLM chains, enabling efficient distributed computation for various tasks.
+Rakshak (Multimodal AI based Emergency Management System) [Role: AI ML & Backend] March 2024
+SPIT’24 repo: Rakshak
+•Enabled quick access to essential services via minimalistic app design andwidget interfaces .
+•Automated emergency categorization via audio or text input utilizing OpenAI Whisper and Google Gemini ,
+reducingresponsetimefrom hourstominutes byswiftlydispatchingdetailstoemergencyservicesandcontacts.
+•Implemented AI-driven resource allocation based on severity and nearby service capacity.
+•WhatsApp bot feature allows users to upload images for immediate life-saving first-aid steps .
+Achievements
+Aeravat 1.0 Second Runner Up in IEEE CS National Level Hackathon 2024
+SPIT Hackathon 24 Runner Up in National Level Hackathon 2024
+TSEC Hack’s 24 3rd Place andML-Winner in State Level Hackathon held at TSEC 2024
+Codeissance’23 Placed1stin this offline hackathon, held at TSEC 2023
+Extracurricular & Certifcations
+•IBM SkillsBuild and CSRBOX Academic Internship - Certificates"""
